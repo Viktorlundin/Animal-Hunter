e4bef26e78f6677f59be06d3e063caf9b4852039
Erik Hällkvist <erik.hallkvist@hotmail.se>
2017-03-31 15:13:42 +0200

    jobbat på servern

--- PhaserTypeScript/Nodejs/server.js
+++ PhaserTypeScript/Nodejs/server.js
@@ -5,12 +5,14 @@ var app = express();
 var server = require('http').createServer(app);
 //socket.io
 var io = require('socket.io')(server);
+//path för att hitta filväg utan säkerhetsproblem för servern(för att hitta index.html i annan map)
+var path = require('path');
 //Allt i denna mappen localhost:port/includes är public för den som besöker hemsidan,
 //där kan vi lägga jquery libary etc.
-app.use(express.static(__dirname + '/includes'));
+app.use(express.static(__dirname + '/../'));
 //Skickar hemsidan index.html när någon besöker hemsidan.
 app.get('/', function (req, res, next) {
-    res.sendFile(__dirname + '/index.html');
+    res.sendFile(path.resolve(__dirname + '/../PhaserTypeScript/index.html'));
 });
 server.listen(1337); //Lysnar på trafik på port 1337
 //# sourceMappingURL=server.js.map
