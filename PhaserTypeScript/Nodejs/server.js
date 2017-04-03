1575037dad7c5b1fc0072f2233c4722cb2521f76
Erik Hällkvist <erik.hallkvist@hotmail.se>
2017-04-03 14:11:20 +0200

    SERVER UPPDATERING ERIK

--- PhaserTypeScript/Nodejs/server.js
+++ PhaserTypeScript/Nodejs/server.js
@@ -9,10 +9,40 @@ var io = require('socket.io')(server);
 var path = require('path');
 //Allt i denna mappen localhost:port/includes är public för den som besöker hemsidan,
 //där kan vi lägga jquery libary etc.
-app.use(express.static(__dirname + '/../'));
+app.use(express.static(__dirname + '/../PhaserTypeScript/'));
 //Skickar hemsidan index.html när någon besöker hemsidan.
 app.get('/', function (req, res, next) {
     res.sendFile(path.resolve(__dirname + '/../PhaserTypeScript/index.html'));
 });
 server.listen(1337); //Lysnar på trafik på port 1337
+//socket.io server
+var SocketServer = (function () {
+    function SocketServer() {
+    }
+    SocketServer.prototype.setEventHandlers = function () {
+        io.on("connection", function (client) {
+            console.log("New player has connected: " + client.id);
+            //client.on("move player", onMovePlayer);
+            //client.on("disconnect", onClientDisconnect);
+            //client.on("place bomb", onPlaceBomb);
+            //client.on("register map", onRegisterMap);
+            //client.on("start game on server", onStartGame);
+            //client.on("ready for round", onReadyForRound);
+            //client.on("powerup overlap", onPowerupOverlap);
+            //client.on("enter lobby", Lobby.onEnterLobby);
+            //client.on("host game", Lobby.onHostGame);
+            //client.on("select stage", Lobby.onStageSelect);
+            //client.on("enter pending game", Lobby.onEnterPendingGame);
+            //client.on("leave pending game", Lobby.onLeavePendingGame);
+        });
+    };
+    SocketServer.prototype.broadcastingLoop = function () {
+        for (var i in game.players) {
+        }
+    };
+    return SocketServer;
+})();
+var SS = new SocketServer();
+SS.setEventHandlers();
+SS.broadcastingLoop();
 //# sourceMappingURL=server.js.map
--- PhaserTypeScript/Nodejs/server.ts
+++ PhaserTypeScript/Nodejs/server.ts
@@ -12,7 +12,7 @@ var path = require('path');
 
 //Allt i denna mappen localhost:port/includes är public för den som besöker hemsidan,
 //där kan vi lägga jquery libary etc.
-app.use(express.static(__dirname + '/../'));
+app.use(express.static(__dirname + '/../PhaserTypeScript/'));
 
 //Skickar hemsidan index.html när någon besöker hemsidan.
 app.get('/', function (req, res, next)
@@ -21,3 +21,43 @@ app.get('/', function (req, res, next)
 });
 
 server.listen(1337); //Lysnar på trafik på port 1337
+
+//socket.io server
+
+class SocketServer
+{
+    setEventHandlers()
+    {
+        io.on("connection", function (client)
+        {
+            console.log("New player has connected: " + client.id);
+
+            //client.on("move player", onMovePlayer);
+            //client.on("disconnect", onClientDisconnect);
+            //client.on("place bomb", onPlaceBomb);
+            //client.on("register map", onRegisterMap);
+            //client.on("start game on server", onStartGame);
+            //client.on("ready for round", onReadyForRound);
+            //client.on("powerup overlap", onPowerupOverlap);
+
+            //client.on("enter lobby", Lobby.onEnterLobby);
+            //client.on("host game", Lobby.onHostGame);
+            //client.on("select stage", Lobby.onStageSelect);
+            //client.on("enter pending game", Lobby.onEnterPendingGame);
+            //client.on("leave pending game", Lobby.onLeavePendingGame);
+        });
+    }
+
+
+    broadcastingLoop()
+    {
+        for (var i in game.players)
+        {
+        }
+    }
+}
+
+let SS = new SocketServer();
+
+SS.setEventHandlers();
+SS.broadcastingLoop();
