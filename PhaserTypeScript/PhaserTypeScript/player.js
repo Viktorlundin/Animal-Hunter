7d8e204e9ac3c4dc349d293b84eec950c9682df7
JonathanSanfer <sanfer_sanfer@hotmail.com>
2017-04-07 10:42:41 +0100

    dsfsd

---
+++ PhaserTypeScript/PhaserTypeScript/player.js
@@ -0,0 +1,40 @@
+var Player = (function () {
+    function Player() {
+        this.speed = 150;
+        this.loadPlayer = function () {
+            this.player = game.add.sprite(1000, game.world.height + 100, 'dude');
+            game.physics.arcade.enable(this.player);
+            this.player.body.collideWorldBounds = true;
+            this.player.body.drag.y = 1000;
+            this.player.animations.add('left', [0, 1, 2, 3], 10, true);
+            this.player.animations.add('right', [5, 6, 7, 8], 10, true);
+            cursors = game.input.keyboard.createCursorKeys();
+        };
+        this.movePlayer = function () {
+            this.player.body.velocity.x = 0;
+            this.player.body.velocity.y = 0;
+            if (cursors.left.isDown) {
+                this.player.body.velocity.x = -this.speed;
+                this.player.animations.play('left');
+            }
+            else if (cursors.right.isDown) {
+                this.player.body.velocity.x = this.speed;
+                this.player.animations.play('left');
+            }
+            else if (cursors.down.isDown) {
+                this.player.body.velocity.y = this.speed;
+                this.player.animations.play('left');
+            }
+            else if (cursors.up.isDown) {
+                this.player.body.velocity.y = -this.speed;
+                this.player.animations.play('left');
+            }
+            else {
+                this.player.animations.stop();
+                this.player.frame = 0;
+            }
+        };
+    }
+    return Player;
+}());
+//# sourceMappingURL=player.js.map
---
+++ PhaserTypeScript/PhaserTypeScript/player.ts
@@ -0,0 +1,48 @@
+﻿class Player
+{
+    constructor() { }
+    speed: any = 150;
+    player: any;
+    loadPlayer = function ()
+    {
+        this.player = game.add.sprite(1000, game.world.height + 100, 'dude');
+        game.physics.arcade.enable(this.player);
+        this.player.body.collideWorldBounds = true;
+        this.player.body.drag.y = 1000;
+        this.player.animations.add('left', [0, 1, 2, 3], 10, true);
+        this.player.animations.add('right', [5, 6, 7, 8], 10, true);
+        cursors = game.input.keyboard.createCursorKeys();
+    }
+
+    movePlayer = function ()
+    {
+        this.player.body.velocity.x = 0;
+        this.player.body.velocity.y = 0;
+
+        if (cursors.left.isDown)
+        {
+            this.player.body.velocity.x = -this.speed;
+            this.player.animations.play('left');
+        }
+        else if (cursors.right.isDown)
+        {
+            this.player.body.velocity.x = this.speed;
+            this.player.animations.play('left');
+        }
+        else if (cursors.down.isDown)
+        {
+            this.player.body.velocity.y = this.speed;
+            this.player.animations.play('left');
+        }
+        else if (cursors.up.isDown)
+        {
+            this.player.body.velocity.y = -this.speed;
+            this.player.animations.play('left');
+        }
+        else
+        {
+            this.player.animations.stop();
+            this.player.frame = 0;
+        }
+    }
+}
