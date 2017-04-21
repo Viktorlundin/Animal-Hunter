558b8df120fb356a5455f3d72cb67ce9ce672669
ErikHallkvist <erik.hallkvist@hotmail.se>
2017-04-21 13:11:55 +0200

    lol

---
+++ PhaserTypeScript/PhaserTypeScript/States/Boot.ts
@@ -0,0 +1,16 @@
+﻿module JungleHunter {
+    export class Boot extends Phaser.State {
+ 
+
+
+
+        create() {
+            console.log("works");
+            this.stage.setBackgroundColor(0xFFFFFF);
+            //this.setEventHandlers();
+            this.game.state.start('Preloader', true, false);
+            
+
+        }
+    }
+}
